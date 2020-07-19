80f9148057e8f43b58e521727f3fcf645d55fd1f
LaurelShark <yarikhaker@gmail.com>
2020-07-19 15:06:09 +0300

    CLT-2: Make login button inactive while attempting login
    Also covers CLT-1, which is to handle login attempt

--- src/views/account/Login.vue
+++ src/views/account/Login.vue
@@ -2,21 +2,27 @@
     <div>
         <auth-header></auth-header>
         <div class="container registration">
-            <h1 class="title">Login</h1>
-            <section class="box" lang="en">
-                <b-field>
-                    <b-input placeholder="Email" type="email" v-model="data.email" required></b-input>
-                </b-field>
+            <form name="loginForm" @submit.prevent="login">
+                <h1 class="title">Login</h1>
+                <section class="box" lang="en">
+                    <b-field>
+                        <b-input placeholder="Email" type="email" v-model="data.email" required></b-input>
+                    </b-field>
 
-                <b-field>
-                    <b-input placeholder="Password" required v-model="data.password" type="password" password-reveal></b-input>
-                </b-field>
+                    <b-field>
+                        <b-input placeholder="Password" required v-model="data.password" type="password"
+                                 password-reveal></b-input>
+                    </b-field>
 
-                <div class="buttons">
-                    <b-button v-on:click="login" type="is-primary" expanded>Login</b-button>
-                </div>
-                <router-link to="/restore">Forgot password?</router-link>
-            </section>
+                    <div class="buttons">
+                        <b-button v-on:click="login" :disabled="loading" type="is-primary" expanded>
+                            <span v-show="loading" class="spinner-border spinner-border-sm"></span>
+                            <span>Login</span>
+                        </b-button>
+                    </div>
+                    <router-link to="/restore">Forgot password?</router-link>
+                </section>
+            </form>
         </div>
     </div>
 </template>
@@ -24,6 +30,8 @@
 <script>
     import accountMixin from "../../mixins/account";
     import AuthHeader from "../../components/shared/AuthHeader";
+    import api from "../../api/api";
+    import authenticationStore from "../../store/authenticationStore";
 
     export default {
         name: "Login",
@@ -36,12 +44,25 @@
                 data: {
                     email: '',
                     password: ''
-                }
+                },
+                loading: false
             }
         },
         methods: {
             login: function () {
                 this.authenticate(this.data)
+            },
+            authenticate: function (data) {
+                this.loading = true;
+                api.account.authenticate(data).then((response) => {
+                        authenticationStore.commit('setToken', response.token)
+                        //@ts-ignore
+                        this.$router.push('/account');
+                    },
+                    error => {
+                        this.loading = false;
+                        this.$router.push('/login')
+                    })
             }
         }
     }
