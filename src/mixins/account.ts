710c0a4a8405b8c15c6c204067ea7ed0ed6a5506
Yaroslav Kladko <yarikhaker@gmail.com>
2020-07-23 01:55:38 +0300

    CLT-2: Make login button inactive while attempting login (#13)
    * CLT-2: Make login button inactive while attempting login
    
    Also covers CLT-1, which is to handle login attempt
    
    * [CLT-2] Make login button inactive while attempting login
    
    Refactored usage of methods
    
    ## Build status action ignored due to misconfiguration, should work for future PRs

--- src/mixins/account.ts
+++ src/mixins/account.ts
@@ -4,15 +4,8 @@ import accountStore from '../store/accountStore'
 
 const accountMixin = {
     methods: {
-        authenticate(data) {
-            api.account.authenticate(data).then((response: any) => {
-                authenticationStore.commit('setToken', response.token)
-                //@ts-ignore
-                this.$router.push('/account');
-            })
-        },
         createAccount(data: any) {
-            api.account.createAccount(data).then((response:any) => {
+            api.account.createAccount(data).then((response: any) => {
                 authenticationStore.commit('setToken', response.authentication.token)
                 accountStore.commit('set', response);
                 //@ts-ignore
